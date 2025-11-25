960b1cd6fd580f81ed54f03388df89786ba45563
makieancla <anclamcfrancis037@gmail.com>
2025-11-25 08:12:13 +0800

    added commit

--- array.js
+++ array.js
@@ -21,8 +21,43 @@ class Arry{
 
         console.log(output);
     }
-}
 
+    insert(index, element){
+        //correcting Statement
+        if (index <o || index > this.length){
+            console.log("Invalid Index");
+            return;
+        }
+
+        //shifting condition
+        for(let i=this.length; i>index; i--){
+            this.data[i] = this.data[i-1];
+        }
+
+        //after condition
+        this.data[index] = element;
+        this.length++;
+    }
+
+    delete(index){
+       insert(index, element){
+        //correcting Statement
+        if (index <o || index > this.length){
+            console.log("Invalid Index");
+            return;
+        } 
+
+         //shifting condition
+        for(let i=this.length; i<index; i++){
+            this.data[i] = this.data[i-1];
+        }
+
+        //AFTER conditon
+        delete this.data[this.length -1];
+        this.length--;
+    }
+    
+    
 //
 
 arr1 =new Arry(4,2,3);

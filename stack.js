01e18300d96ac03b75eb6c38ad5bc800543ded33
makieancla <anclamcfrancis037@gmail.com>
2025-11-25 08:53:17 +0800

    added done

--- stack.js
+++ stack.js
@@ -24,18 +24,28 @@ class Stack{
         //correcting statement
         return this.data[this.topIndex];
     }
+
+    pop(){
+        //correcting statement
+        if(this.is_empty()) return nullll
+         let value = this.data[this.topIndex];
+        delete this.data[this.topIndex];
+        this.topIndex--;
+        return value;
+
+    }
 }
 //
 
 let stack1 = new Stack();
-
-console.log(stack1.is_empty());
-stack.empty()
+    
 stack1.push(4);
 stack1.push(2);
 stack1.push(3);
 stack1.push(5);
 stack1.push(8);
-console.log(stack1.is_empty());
-console.log(stack1.size());
+console.log("the current size is" +stack1.is_empty());
+console.log("the top element is" +stack1.size());
 console.log("The top element"+stack1.peek());
+console.log("Top element "+ stack1.pop() + " is popped, the new top element is " + stack1.peek());
+console.log("the new stack size is" +stack1.size());
